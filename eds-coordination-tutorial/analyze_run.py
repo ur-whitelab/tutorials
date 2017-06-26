5cd51f01bdde5fe98ccbf0c90fea1f503db1f0d1
Andrew White <white.d.andrew@gmail.com>
2017-06-26 17:19:11 -0400

    More work on coord-number

--- eds-coordination-tutorial/analyze_run.py
+++ eds-coordination-tutorial/analyze_run.py
@@ -40,13 +40,14 @@ def plot_temp(eners, labels, ax, indices=[0,3]):
         data = np.genfromtxt(e)
         ax.plot(data[:,indices[0]], data[:,indices[1]], label=l)
     ax.legend(loc='best')
+    ax.set_ylim(250,350)
 
 
 
 def plot_cv(cns, labels, set_points):
     '''plt the coordination numbers'''    
-    N = len(cns)
-    fig, axes = plt.subplots(N,sharex=True, sharey=True, figsize=(5,4 + 2.5 * (N - 1)))
+    N = len(set_points)
+    fig, axes = plt.subplots(N,sharex=True, sharey=True, figsize=(5,2 + 1.5 * (N - 1)))
     try:
         iter(axes)
     except TypeError:
@@ -55,12 +56,53 @@ def plot_cv(cns, labels, set_points):
     for i,(e,l) in enumerate(zip(cns, labels)):
         data = np.genfromtxt(e, skip_footer=1)
         for j,ax in enumerate(axes):
-            ax.plot(data[:,0], data[:,i+1] / set_points[i], label=l, color=cmap(i / N))
-    axes[0].legend(loc='best')
+            ax.plot(data[:,0] / 1000, data[:,j+1] / set_points[j], label=l, color=cmap(i / N))
+            #ax.set_ylim(0,2)
+    for p,ax in zip(set_points, axes):
+        ax.axhline(y=1.0, linestyle='--', color='blue')
+        #ax.get_yaxis().set_ticks([0,0.5,1,1.5])
+    axes[0].legend(loc='best')    
     fig.subplots_adjust(hspace=0)
     plt.setp([a.get_xticklabels() for a in fig.axes[:-1]], visible=False)
+
+    
+def plot_bias(eds_files,labels, N, M=8):
+    '''plot the biases'''
+
+    #we will plot every other row to avoid to 0s in the mean
+    fig, axes = plt.subplots(N,M, sharex=True, figsize=(5 + 3.5 * (M - 1),2 + 1.5 * (N - 1)))
+    
+    for i, (e, l) in enumerate(zip(eds_files, labels)):
+        with open(e) as f:
+            fields_str = f.readline()[2:-1]
+            column_labels = fields_str.split()[1:]
+            column_dict = {k: i for i,k in enumerate(column_labels)}
+        data = np.genfromtxt(e, skip_footer=1)
+        for (i,j), ax in np.ndenumerate(axes):
+            try:
+                #maybe we aren't biasing all moments
+                name = column_labels[i * M + j + 1]
+            except IndexError:
+                continue
+            col = data[:, i * M + j + 1]#add one to skip time
+            #for means, remove 0s
+            if(name.split('_')[-1] == 'mean' or name.split('_')[-1] == 'std'):
+                ax.plot(data[col != 0, column_dict['time']] / 1000, col[col != 0], label=l)
+            else:
+                ax.plot(data[:, column_dict['time']] / 1000, col, label=l)
+            if i != N - 1:
+                plt.setp(ax.get_xticklabels(), visible=False)
+            if i == 0:
+                ax.set_title(name.split('_')[-1])
+
+            
+    axes[0,0].legend(loc='best')    
+    fig.subplots_adjust(hspace=0)
+
+        
         
 
+    
 
 def main():
     import sys, os
@@ -68,14 +110,18 @@ def main():
     print('Analyzing ', ','.join(names))
     eners = ['{}.ener'.format(n) for n in names]
     cns = ['{}_cn.log'.format(n) for n in names]
+    eds = ['{}_eds.restart'.format(n) for n in names[1:]]
     trajs = ['{}_pos.xyz'.format(n) for n in names]
     rdfs = ['{}.rdf'.format(n) for n in names]
 
     #read in set-points
+    set_points = []
     with open('set_points.txt', 'r') as f:
-        set_points = [float(x[:-1]) for x in f.readlines()]
+        for line in f.readlines():
+            if(not line.startswith('#')):
+                set_points.append(float(line[:-1]))
 
-    mpl.style.use('seaborn-muted')
+    mpl.style.use(['seaborn-muted', 'seaborn-white'])
     mpl.rcParams['savefig.dpi'] = 300
 
 
@@ -105,6 +151,11 @@ def main():
 
     plot_cv(cns, names, set_points)
     plt.savefig('cn_comparison.png')
+
+    #skip first (unbiased)
+    plot_bias(eds, names[1:], len(set_points))
+    plt.savefig('biases_comparison.png')
+
     
     
 if __name__ == '__main__':
