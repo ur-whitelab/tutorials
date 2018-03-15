8c8d0b87407c5667975d6ca02e0081a15ed695f9
Andrew White <andrew.white@rochester.edu>
2018-03-15 17:16:07 -0400

    Updated python analysis script for new version

--- eds-coordination-tutorial/analyze_run.py
+++ eds-coordination-tutorial/analyze_run.py
@@ -10,7 +10,7 @@ import numpy as np
 
 def plot_traj_rdf(trajs, labels, ax):
     '''Plot the RDF'''
-    
+
     for t,l in zip(trajs, labels):
         u = MDAnalysis.Universe(t)
         g = u.select_atoms('name O')
@@ -18,7 +18,7 @@ def plot_traj_rdf(trajs, labels, ax):
         rdf.run()
         print(rdf.rdf)
         ax.plot(rdf.bins, rdf.rdf, label=l)
-        
+
     ax.legend(loc='best')
     ax.set_xlabel('$r$ [Å]')
     ax.set_ylabel('$g(r)$')
@@ -26,20 +26,20 @@ def plot_traj_rdf(trajs, labels, ax):
 
 def plot_rdf(rdfs, labels, ax):
     '''Plot the RDF'''
-    
+
     for t,l in zip(rdfs, labels):
         data = np.genfromtxt(t, skip_header=4)
-        ax.plot(data[:,1], data[:,2], label=l)        
+        ax.plot(data[:,1], data[:,2], label=l)
     ax.legend(loc='best')
     ax.set_xlabel('$r$ [Å]')
     ax.set_ylabel('$g(r)$')
     plt.tight_layout()
-    
+
 
 def plot_temp(eners, labels, ax, indices=[0,3]):
     '''plot the temperature over time'''
     for e, l in zip(eners, labels):
-        data = np.genfromtxt(e)
+        data = np.genfromtxt(e, invalid_raise=False)
         ax.plot(data[:,indices[0]], data[:,indices[1]], label=l)
     ax.legend(loc='best')
     ax.set_ylim(250,350)
@@ -47,60 +47,59 @@ def plot_temp(eners, labels, ax, indices=[0,3]):
 
 
 def plot_cv(cns, labels, set_points):
-    '''plt the coordination numbers'''    
+    '''plt the coordination numbers'''
     N = len(set_points)
     fig, axes = plt.subplots(N,sharex=True, sharey=True, figsize=(5,2 + 1.5 * (N - 1)))
     try:
         iter(axes)
     except TypeError:
         axes = [axes]
-    cmap=plt.cm.get_cmap('Accent', N)
     for i,(e,l) in enumerate(zip(cns, labels)):
-        data = np.genfromtxt(e, skip_footer=1)
+        data = np.genfromtxt(e, skip_footer=1, invalid_raise=False)
         for j,ax in enumerate(axes):
-            ax.plot(data[:,0] / 1000, data[:,j+1] / set_points[j], label=l, color=cmap(i / N))
+            ax.plot(data[:,0] / 1000, data[:,j+1] / set_points[j], label=l, color='C{}'.format(i))
             #ax.set_ylim(0,2)
     for p,ax in zip(set_points, axes):
         ax.axhline(y=1.0, linestyle='--', color='blue', label='set-point')
         #ax.get_yaxis().set_ticks([0,0.5,1,1.5])
-    axes[0].legend(loc='best')    
+    axes[0].legend(loc='best')
     fig.subplots_adjust(hspace=0)
     plt.setp([a.get_xticklabels() for a in fig.axes[:-1]], visible=False)
 
-    
-def plot_bias(eds_files,labels, N, M=8):
+
+def plot_bias(eds_files,labels, N, M=9):
     '''plot the biases'''
 
     #we will plot every other row to avoid to 0s in the mean
     fig, axes = plt.subplots(N,M, sharex=True, figsize=(5 + 3.5 * (M - 1),2 + 1.5 * (N - 1)))
-    
+
     for i, (e, l) in enumerate(zip(eds_files, labels)):
         with open(e) as f:
             fields_str = f.readline()[2:-1]
             column_labels = fields_str.split()[1:]
             column_dict = {k: i for i,k in enumerate(column_labels)}
-        data = np.genfromtxt(e, skip_footer=1)
-        for (i,j), ax in np.ndenumerate(axes):
+        data = np.genfromtxt(e, skip_footer=1, invalid_raise=False)
+        for (j,k), ax in np.ndenumerate(axes):
             try:
                 #maybe we aren't biasing all moments
-                name = column_labels[i * M + j + 1]
+                name = column_labels[j * M + k + 1]
             except IndexError:
                 continue
-            col = data[:, i * M + j + 1]#add one to skip time
+            col = data[:, j * M + k + 1]#add one to skip time
             #for means, remove 0s
             if(name.split('_')[-1] == 'mean' or name.split('_')[-1] == 'std'):
-                ax.plot(data[col != 0, column_dict['time']] / 1000, col[col != 0], label=l)
+                ax.plot(data[col != 0, column_dict['time']] / 1000, col[col != 0], label=l, color='C{}'.format(i))
             else:
-                ax.plot(data[:, column_dict['time']] / 1000, col, label=l)
-            if i != N - 1:
+                ax.plot(data[:, column_dict['time']] / 1000, col, label=l, color='C{}'.format(i))
+            if j != N - 1:
                 plt.setp(ax.get_xticklabels(), visible=False)
-            if i == 0:
+            if j == 0:
                 ax.set_title(name.split('_')[-1])
 
-            
-    axes[0,0].legend(loc='best')    
-    fig.subplots_adjust(hspace=0)        
-    
+
+    axes[0,0].legend(loc='best')
+    fig.subplots_adjust(hspace=0)
+
 
 def main():
     import sys, os
@@ -132,17 +131,17 @@ def main():
     ax.plot(data[:,0], data[:,1], label='Experiment [Skinner et al.]')
     ax.set_xlim(0,10)
 
-    if os.path.exists(rdfs[0]):        
+    if os.path.exists(rdfs[0]):
         plot_rdf(rdfs, names, ax)
     elif os.path.exists(trajs[0]):
         plot_traj_rdf(trajs, names, ax)
     else:
         raise NameError('Could not find trajs or rdfs')
-    
+
     plt.savefig('rdf_comparison.png')
-                
+
     plt.figure(figsize=(4,3))
-    ax = plt.gca()    
+    ax = plt.gca()
     ax.set_title('Temperature')
     plot_temp(eners, names, ax)
     plt.savefig('temperature_comparison.png')
@@ -154,7 +153,7 @@ def main():
     plot_bias(eds, names[1:], len(set_points))
     plt.savefig('biases_comparison.png')
 
-    
-    
+
+
 if __name__ == '__main__':
     main()
